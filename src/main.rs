d390ca9fdcc1e997754e59b9bdcf57e716f4c535
Víctor Javier Díaz Garrido <vicdigar@hotmail.com>
2021-01-29 10:09:46 +0100

    Added uptime support
    Signed-off-by: Víctor Javier Díaz Garrido <vicdigar@hotmail.com>

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,18 @@ mod cpu;
 mod os;
 mod motherboard;
 mod kernel;
+mod uptime;
 
 use cpu::cpu_info;
 use os::os_info;
 use motherboard::motherboard_info;
 use kernel::kernel_info;
+use uptime::uptime_info;
 
 fn main() {
     println!("OS: {}", os_info());
     println!("Host: {}", motherboard_info());
     println!("Kernel: {}", kernel_info());
+    println!("Uptime: {}", uptime_info());
     println!("CPU: {}", cpu_info());
 }
---
+++ src/uptime.rs
@@ -0,0 +1,23 @@
+use std::process::Command;
+
+fn parse_uptime_info(info: String) -> String {
+    let upt_info: Vec<&str> = info.split("up ").collect();
+    let res = upt_info[1].to_string();
+
+    res
+}
+
+fn pretty_uptime_info(info: String) -> String{
+    let uptime_info = &info;
+    let res = format!("{}", uptime_info.trim_end());
+
+    res
+}
+
+pub fn uptime_info() -> String {
+    let uptime_info_command = Command::new("uptime").arg("-p").output().expect("Failed to execute uptime command");
+    let uptime_info = parse_uptime_info(String::from_utf8_lossy(&uptime_info_command.stdout).to_string());
+    let res = pretty_uptime_info(uptime_info);
+
+    res
+}
