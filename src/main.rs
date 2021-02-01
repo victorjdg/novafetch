ab74e0b93a5250a263319260b926019fb832b944
Víctor Javier Díaz Garrido <vicdigar@hotmail.com>
2021-02-01 12:43:15 +0100

    Added initial support for options
    Signed-off-by: Víctor Javier Díaz Garrido <vicdigar@hotmail.com>

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ mod kernel;
 mod uptime;
 mod gpu;
 
+
+use std::env;
 use cpu::cpu_info;
 use os::os_info;
 use motherboard::motherboard_info;
@@ -13,10 +15,26 @@ use uptime::uptime_info;
 use gpu::gpu_info;
 
 fn main() {
-    println!("OS: {}", os_info());
-    println!("Host: {}", motherboard_info());
-    println!("Kernel: {}", kernel_info());
-    println!("Uptime: {}", uptime_info());
-    println!("CPU: {}", cpu_info());
-    println!("GPU: {}", gpu_info());
+    let options: Vec<String> = env::args().collect();
+    if options.len() > 1 {
+        let valid_options = options[2 .. options.len()].to_vec();
+        for option in valid_options {
+            match option.as_str() {
+                "-o" | "-O" | "-os" | "-OS" => println!("OS: {}", os_info()),
+                "-h" | "-H" | "-host" | "-HOST" => println!("Host: {}", motherboard_info()),
+                "-k" | "-K" | "-kernel" | "-KERNEL" => println!("Kernel: {}", kernel_info()),
+                "-u" | "-U" | "-uptime" | "-UPTIME" => println!("Uptime: {}", uptime_info()),
+                "-c" | "-C" | "-cpu" | "-CPU" => println!("CPU: {}", cpu_info()),
+                "-g" | "-G" | "-gpu" | "-GPU" => println!("GPU: {}", gpu_info()),
+                _ => println!("Invalid option {}", option),
+            }
+        }
+    } else {
+        println!("OS: {}", os_info());
+        println!("Host: {}", motherboard_info());
+        println!("Kernel: {}", kernel_info());
+        println!("Uptime: {}", uptime_info());
+        println!("CPU: {}", cpu_info());
+        println!("GPU: {}", gpu_info());
+    }
 }
